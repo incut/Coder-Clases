af0be80860c7a1b5ed89a8927862f3c8b4e07f68
incut <augustopresfraind@gmail.com>
2024-06-04 21:04:35 -0300

    Playground parte 2

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from inicio.models import Auto
 
+admin.site.register(Auto)
 # Register your models here.
--- inicio/models.py
+++ inicio/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Auto(models.Model):
     modelo = models.CharField(max_length=20)
     marca = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f'soy el auto {self.marca} {self.modelo}'
 # Create your models here.
